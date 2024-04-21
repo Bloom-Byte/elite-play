9bb4f799d66ba3291a634c1cba0ebecb603cddec
Modupe Akanni <modupe775@gmail.com>
2024-04-21 08:36:20 +0100

    update top nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,17 +23,16 @@ const Navbar = ({ isNavOpen, user }) => {
   const [vipSupport, setVipSupport] = useState(false);
   const [languagePopup, setLanguagePopup] = useState(false);
 
-  
   const location = useLocation();
   const isReferralsPage = location.pathname === '/referrals';
-  const isDicePage = location.pathname === '/dice'
-  const isCrashPage = location.pathname == '/crash'
+  const isDicePage = location.pathname === '/dice';
+  const isCrashPage = location.pathname == '/crash';
 
   return (
     <>
       <div className="nav">
         <div className={`nav-games ${isNavOpen ? 'nav-expanded' : ''}`}>
-          <div style={{display: 'none'}} className="sidenav__icon">
+          <div style={{ display: 'none' }} className="sidenav__icon">
             <img
               className="nav-icon"
               onClick={() => setIsMobileNavOpen(!isMobileNavOpen)}
@@ -42,153 +41,154 @@ const Navbar = ({ isNavOpen, user }) => {
             />
             <a href="/">
               {' '}
-              <img className='nav-logo' src="./eliteplay.svg" alt="logo" />
+              <img className="nav-logo" src="./eliteplay.svg" alt="logo" />
             </a>
           </div>
-          <div className="nav-games__dice">
-            <img src="./dice.svg" alt="dice-logo" />
-            <span>
-              <a href="/dice">Dice</a>
-            </span>
-          </div>
-          <div className="nav-games__dice">
-            <img src="./chart-increase.svg" alt="chart-logo" />
-            <span>
-              <a href="/crash">Crash</a>
-            </span>
-          </div>
+          <a style={{ textDecoration: 'none' }} href="/dice">
+            <div className="nav-games__dice">
+              <img src="./dice.svg" alt="dice-logo" />
+              <span>Dice</span>
+            </div>
+          </a>
+
+          <a style={{ textDecoration: 'none' }} href="/crash">
+            <div className="nav-games__dice">
+              <img src="./chart-increase.svg" alt="chart-logo" />
+              <span>Crash</span>
+            </div>
+          </a>
         </div>
         {isMobileNavOpen && (
-        <div className="sidenav-expanded">
-          <div className="sidenav__icon">
-            <img
-              className="nav-icon"
-              onClick={() => setIsMobileNavOpen(!isMobileNavOpen)}
-              src="./menu-01.svg"
-              alt="close-icon"
-            />
-            <a href="/">
-              {' '}
-              <img src="./eliteplay.svg" alt="logo" />
-            </a>
-          </div>
-          <div className="sidenav__links">
-            <div
-              style={{
-                backgroundColor: isReferralsPage ? '#0B281F' : 'transparent',
-                padding: isReferralsPage ? '10px' : '0',
-                borderRadius: isReferralsPage ? '9px' : '0',
-                transition: 'background-color 0.3s, padding 0.3s',
-              }}
-            >
-              <a style={{ textDecoration: 'none' }}  href="/referrals">
-                <div className="sidenav__link">
-                  <img src="./gift.svg" alt="reward-icon" />
-                  <span>Refer and Earn</span>
-                </div>
+          <div className="sidenav-expanded">
+            <div className="sidenav__icon">
+              <img
+                className="nav-icon"
+                onClick={() => setIsMobileNavOpen(!isMobileNavOpen)}
+                src="./menu-01.svg"
+                alt="close-icon"
+              />
+              <a href="/">
+                {' '}
+                <img src="./eliteplay.svg" alt="logo" />
               </a>
             </div>
-            <div
-              style={{
-                backgroundColor: isDicePage ? '#0B281F' : 'transparent',
-                padding:isDicePage ? '10px' : '0',
-                borderRadius: isDicePage ? '9px' : '0',
-                transition: 'background-color 0.3s, padding 0.3s',
-              }}
-            >
-              <a style={{ textDecoration: 'none' }} href="/dice">
-                <div className="sidenav__link">
-                  <img src="./dice.svg" alt="dice-icon" />
-                  <span>Dice</span>
-                </div>
-              </a>
-            </div>
-            <div
-              style={{
-                backgroundColor: isCrashPage ? '#0B281F' : 'transparent',
-                padding: isCrashPage ? '10px' : '0',
-                borderRadius: isCrashPage ? '9px' : '0',
-                transition: 'background-color 0.3s, padding 0.3s',
-              }}
-            >
-              <a style={{ textDecoration: 'none' }} href="/crash">
-                <div className="sidenav__link">
-                  <img src="./chart-increase.svg" alt="crash-icon" />
-                  <span>Crash</span>
-                </div>
-              </a>
-            </div>
-            <div
-              style={{
-                backgroundColor: chatOpen ? '#0B281F' : 'transparent',
-                padding: chatOpen ? '10px' : '0',
-                borderRadius: chatOpen ? '9px' : '0',
-                transition: 'background-color 0.3s, padding 0.3s',
-              }}
-            >
+            <div className="sidenav__links">
               <div
-                onClick={() => setChatOpen(!chatOpen)}
-                className="sidenav__link"
-              >
-                <img src="./message-01.svg" alt="chat-icon" />
-                <span>Chatroom</span>
-              </div>
-            </div>
-            <div
-              style={{
-                backgroundColor: vipSupport ? '#0B281F' : 'transparent',
-                padding: vipSupport ? '10px' : '0',
-                borderRadius: vipSupport ? '9px' : '0',
-                transition: 'background-color 0.3s, padding 0.3s',
-              }}
-            >
-              <div
-                onClick={() => setVipSupport(!vipSupport)}
-                className="sidenav__link"
-              >
-                <img src="./VIP.svg" alt="vip-icon" />
-                <span>
-                  <span className="sidenav-vip">VIP</span> Club
-                </span>
-              </div>
-            </div>
-            <div
-              style={{
-                backgroundColor: liveSupport ? '#0B281F' : 'transparent',
-                padding: liveSupport ? '10px' : '0',
-                borderRadius: liveSupport ? '9px' : '0',
-                transition: 'background-color 0.3s, padding 0.3s',
-              }}
-            >
-              <div
-                onClick={() => setLiveSupport(!liveSupport)}
-                className="sidenav__link"
-              >
-                <img src="./customer-support.svg" alt="support-icon" />
-                <span>Live Support</span>
-              </div>
-            </div>
-            <div
-              style={{
-                backgroundColor: languagePopup ? '#0B281F' : 'transparent',
-                padding: languagePopup ? '10px' : '0',
-                borderRadius: languagePopup ? '9px' : '0',
-                transition: 'background-color 0.3s, padding 0.3s',
-              }}
-            >
-              <div
-                onClick={() => {
-                  setLanguagePopup(!languagePopup);
+                style={{
+                  backgroundColor: isReferralsPage ? '#0B281F' : 'transparent',
+                  padding: isReferralsPage ? '10px' : '0',
+                  borderRadius: isReferralsPage ? '9px' : '0',
+                  transition: 'background-color 0.3s, padding 0.3s',
                 }}
-                className="sidenav__link"
               >
-                <img src="./translate.svg" alt="translate-icon" />
-                <span>Language: English</span>
+                <a style={{ textDecoration: 'none' }} href="/referrals">
+                  <div className="sidenav__link">
+                    <img src="./gift.svg" alt="reward-icon" />
+                    <span>Refer and Earn</span>
+                  </div>
+                </a>
+              </div>
+              <div
+                style={{
+                  backgroundColor: isDicePage ? '#0B281F' : 'transparent',
+                  padding: isDicePage ? '10px' : '0',
+                  borderRadius: isDicePage ? '9px' : '0',
+                  transition: 'background-color 0.3s, padding 0.3s',
+                }}
+              >
+                <a style={{ textDecoration: 'none' }} href="/dice">
+                  <div className="sidenav__link">
+                    <img src="./dice.svg" alt="dice-icon" />
+                    <span>Dice</span>
+                  </div>
+                </a>
+              </div>
+              <div
+                style={{
+                  backgroundColor: isCrashPage ? '#0B281F' : 'transparent',
+                  padding: isCrashPage ? '10px' : '0',
+                  borderRadius: isCrashPage ? '9px' : '0',
+                  transition: 'background-color 0.3s, padding 0.3s',
+                }}
+              >
+                <a style={{ textDecoration: 'none' }} href="/crash">
+                  <div className="sidenav__link">
+                    <img src="./chart-increase.svg" alt="crash-icon" />
+                    <span>Crash</span>
+                  </div>
+                </a>
+              </div>
+              <div
+                style={{
+                  backgroundColor: chatOpen ? '#0B281F' : 'transparent',
+                  padding: chatOpen ? '10px' : '0',
+                  borderRadius: chatOpen ? '9px' : '0',
+                  transition: 'background-color 0.3s, padding 0.3s',
+                }}
+              >
+                <div
+                  onClick={() => setChatOpen(!chatOpen)}
+                  className="sidenav__link"
+                >
+                  <img src="./message-01.svg" alt="chat-icon" />
+                  <span>Chatroom</span>
+                </div>
+              </div>
+              <div
+                style={{
+                  backgroundColor: vipSupport ? '#0B281F' : 'transparent',
+                  padding: vipSupport ? '10px' : '0',
+                  borderRadius: vipSupport ? '9px' : '0',
+                  transition: 'background-color 0.3s, padding 0.3s',
+                }}
+              >
+                <div
+                  onClick={() => setVipSupport(!vipSupport)}
+                  className="sidenav__link"
+                >
+                  <img src="./VIP.svg" alt="vip-icon" />
+                  <span>
+                    <span className="sidenav-vip">VIP</span> Club
+                  </span>
+                </div>
+              </div>
+              <div
+                style={{
+                  backgroundColor: liveSupport ? '#0B281F' : 'transparent',
+                  padding: liveSupport ? '10px' : '0',
+                  borderRadius: liveSupport ? '9px' : '0',
+                  transition: 'background-color 0.3s, padding 0.3s',
+                }}
+              >
+                <div
+                  onClick={() => setLiveSupport(!liveSupport)}
+                  className="sidenav__link"
+                >
+                  <img src="./customer-support.svg" alt="support-icon" />
+                  <span>Live Support</span>
+                </div>
+              </div>
+              <div
+                style={{
+                  backgroundColor: languagePopup ? '#0B281F' : 'transparent',
+                  padding: languagePopup ? '10px' : '0',
+                  borderRadius: languagePopup ? '9px' : '0',
+                  transition: 'background-color 0.3s, padding 0.3s',
+                }}
+              >
+                <div
+                  onClick={() => {
+                    setLanguagePopup(!languagePopup);
+                  }}
+                  className="sidenav__link"
+                >
+                  <img src="./translate.svg" alt="translate-icon" />
+                  <span>Language: English</span>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      )}
+        )}
         {userIsLoggedIn ? (
           <div>
             <div className="nav-loggedin">
@@ -229,7 +229,15 @@ const Navbar = ({ isNavOpen, user }) => {
                 }}
                 className="nav-profile"
               >
-                <img style={{borderRadius: '50%'}} src={`${user?.profilePictureUrl? user.profilePictureUrl : './profile-img.svg'}`} alt="profile-img" />
+                <img
+                  style={{ borderRadius: '50%' }}
+                  src={`${
+                    user?.profilePictureUrl
+                      ? user.profilePictureUrl
+                      : './profile-img.svg'
+                  }`}
+                  alt="profile-img"
+                />
                 <img src="./down-arrow.svg" alt="arrow" />
               </div>
             </div>
